4e0df4bd6c75d5b662437b226b1da9c5c9a48ad4
Edgar Yonemura <edgar.yonemura@gmail.com>
2022-09-04 20:16:00 -0300

    [nvim] Add base options

---
+++ .config/nvim/init.lua
@@ -0,0 +1,22 @@
+-- Nvim options
+-- type :help <options> to get more information
+-- or visit https://neovim.io/doc/user/options.html
+vim.opt.background = "light"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.guifont = "firacode:h16"
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.pumheight = 10
+vim.opt.relativenumber = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = "yes"
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.undofile = true
