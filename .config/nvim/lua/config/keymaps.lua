48754b7b54d317b5b1ed7393c4a91f30ad55097b
Edgar Yonemura <edgar.yonemura@gmail.com>
2022-09-04 21:54:09 -0300

    [nvim] Add keymaps for bufferlines

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,14 @@ vim.g.maplocalleader = " "
 keymap("n", "<leader>ff", ":Files<CR>", options)
 keymap("n", "<leader>fs", ":Rg<CR>", options)
 keymap("n", "<leader>fh", ":History<CR>", options)
+
+-- bufferlines
+keymap("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>", options)
+keymap("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>", options)
+keymap("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>", options)
+keymap("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>", options)
+keymap("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", options)
+keymap("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>", options)
+keymap("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>", options)
+keymap("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", options)
+keymap("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", options)
